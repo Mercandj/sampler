3cfc9010b0138e9fd644f4d622349ac20b22f721
Jonathan <modjow.jm9@gmail.com>
2018-03-10 12:20:15 +0100

    [iOS] :art: Add button A

--- sampler-ios/sampler/ViewController.swift
+++ sampler-ios/sampler/ViewController.swift
@@ -1,25 +1,38 @@
-//
-//  ViewController.swift
-//  sampler
-//
-//  Created by Jonathan on 07/03/2018.
-//  Copyright © 2018 Jonathan. All rights reserved.
-//
-
 import UIKit
+import AVFoundation
+
+var player: AVAudioPlayer?
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let button = UIButton(frame: CGRect(x: 100, y: 100, width: 100, height: 50))
+        button.backgroundColor = .black
+        button.setTitle("A", for: .normal)
+        button.addTarget(self, action: #selector(self.buttonAction(_:)), for: .touchUpInside)
+        self.view.addSubview(button)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @objc func buttonAction(_ sender: UIButton!) {
+        print("Button tapped")
+    }
+    
+    // https://stackoverflow.com/questions/32036146/how-to-play-a-sound-using-swift
+    func playSound() {
+        guard let url = Bundle.main.url(forResource: "soundName", withExtension: "mp3") else { return }
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+            try AVAudioSession.sharedInstance().setActive(true)
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+            guard let player = player else { return }
+            player.play()
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
 }
-
