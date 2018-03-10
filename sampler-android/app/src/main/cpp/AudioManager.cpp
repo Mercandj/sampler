d4f716549d78791d8b649957d41ef1a72af7828d
Jonathan <modjow.jm9@gmail.com>
2018-03-10 06:34:18 +0100

    [Android] :wrench: Bump kotlin. Add native logs. Prepare oboe

--- sampler-android/app/src/main/cpp/AudioManager.cpp
+++ sampler-android/app/src/main/cpp/AudioManager.cpp
@@ -1,13 +1,67 @@
-//
-// Created by Jonathan on 10/03/2018.
-//
-
 #include "AudioManager.h"
+#include "logging_macros.h"
 
-AudioManager::AudioManager() {
+constexpr int32_t kDefaultChannelCount = 2; // Stereo
 
+AudioManager::AudioManager() :
+        mChannelCount(kDefaultChannelCount) {
+    createPlaybackStream();
+}
+
+AudioManager::~AudioManager() {
+    closeOutputStream();
 }
 
 void AudioManager::Play() {
+    LOGD("Play");
+}
 
+void AudioManager::createPlaybackStream() {
+    oboe::AudioStreamBuilder builder;
+    setupPlaybackStreamParameters(&builder);
+    oboe::Result result = builder.openStream(&mPlayStream);
+
+    if (result == oboe::Result::OK && mPlayStream != nullptr) {
+        mSampleRate = mPlayStream->getSampleRate();
+        mFramesPerBurst = mPlayStream->getFramesPerBurst();
+        int channelCount = mPlayStream->getChannelCount();
+        if (channelCount != mChannelCount) {
+            LOGW("Requested %d channels but received %d", mChannelCount, channelCount);
+        }
+        mPlayStream->setBufferSizeInFrames(mFramesPerBurst);
+        result = mPlayStream->requestStart();
+        if (result != oboe::Result::OK) {
+            LOGE("Error starting stream. %s", oboe::convertToText(result));
+        }
+    } else {
+        LOGE("Failed to create stream. Error: %s", oboe::convertToText(result));
+    }
+}
+
+void AudioManager::setupPlaybackStreamParameters(oboe::AudioStreamBuilder *builder) {
+    builder->setAudioApi(mAudioApi);
+    builder->setDeviceId(mPlaybackDeviceId);
+    builder->setChannelCount(mChannelCount);
+    builder->setSharingMode(oboe::SharingMode::Exclusive);
+    builder->setPerformanceMode(oboe::PerformanceMode::LowLatency);
+    builder->setCallback(this);
+}
+
+void AudioManager::closeOutputStream() {
+    if (mPlayStream != nullptr) {
+        oboe::Result result = mPlayStream->requestStop();
+        if (result != oboe::Result::OK) {
+            LOGE("Error stopping output stream. %s", oboe::convertToText(result));
+        }
+        result = mPlayStream->close();
+        if (result != oboe::Result::OK) {
+            LOGE("Error closing output stream. %s", oboe::convertToText(result));
+        }
+    }
+}
+
+oboe::DataCallbackResult
+AudioManager::onAudioReady(oboe::AudioStream *audioStream, void *audioData, int32_t numFrames) {
+    // TODO
+    return oboe::DataCallbackResult::Continue;
 }
--- sampler-android/app/src/main/cpp/AudioManager.h
+++ sampler-android/app/src/main/cpp/AudioManager.h
@@ -1,19 +1,38 @@
-//
-// Created by Jonathan on 10/03/2018.
-//
-
 #ifndef SAMPLER_ANDROID_AUDIOMANAGER_H
 #define SAMPLER_ANDROID_AUDIOMANAGER_H
 
+#include <oboe/Oboe.h>
+#include <oboe/Definitions.h>
+#include <oboe/AudioStream.h>
 
-class AudioManager {
+class AudioManager : oboe::AudioStreamCallback {
 
 public:
 
     AudioManager();
 
+    ~AudioManager();
+
     void Play();
 
+private:
+    oboe::AudioApi mAudioApi = oboe::AudioApi::Unspecified;
+    int32_t mPlaybackDeviceId = oboe::kUnspecified;
+    oboe::AudioStream *mPlayStream;
+
+    int32_t mSampleRate;
+    const int32_t mChannelCount;
+    int32_t mFramesPerBurst;
+
+    void createPlaybackStream();
+
+    void setupPlaybackStreamParameters(oboe::AudioStreamBuilder *builder);
+
+    void closeOutputStream();
+
+    // oboe::StreamCallback methods
+    oboe::DataCallbackResult
+    onAudioReady(oboe::AudioStream *audioStream, void *audioData, int32_t numFrames);
 };
 
 
